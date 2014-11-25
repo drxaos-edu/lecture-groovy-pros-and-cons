053db1b38d801f8b01585953bc2abf70e1c3daa6
xaos <vladimir.p.polyakov@gmail.com>
2014-11-25 12:44:42 +0500

    Initial commit

---
+++ src/main/groovy/objects/Objects.groovy
@@ -0,0 +1,18 @@
+package objects
+
+public class Objects {
+    public static void main(String[] a) {
+
+        println 10.toString()
+
+        println 0.5.movePointRight(1)
+
+        println 8d.isInfinite()
+
+        println true.hashCode()
+
+        println 'x'.toUpperCase()
+
+        println 0xFF.compareTo(256)
+    }
+}
