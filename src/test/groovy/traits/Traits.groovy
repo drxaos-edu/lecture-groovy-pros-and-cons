9688bd45677435d32b10b859dc8e6147f98a233c
xaos <vladimir.p.polyakov@gmail.com>
2014-11-27 02:39:54 +0500

    traits

---
+++ src/test/groovy/traits/Traits.groovy
@@ -0,0 +1,28 @@
+package traits
+
+//trait A {
+//    String exec() { 'A' }
+//}
+//
+//trait B {
+//    String exec() { 'B' }
+//}
+//
+//class C implements A, B {}
+//
+//class D implements A, B {
+//    String exec() { A.super.exec() }
+//}
+
+class Traits extends GroovyTestCase {
+
+
+    void test1() {
+//        println new C().exec()
+    }
+
+
+    void test2() {
+//        println new D().exec()
+    }
+}
